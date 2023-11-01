7c7db9ad299d7f34ab94d1aab0b56a6304e0917f
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2023-11-01 14:39:26 +0700

    คำสั่ง in เพื่อตรวจสอบการเป็นสมาชิกในรายการ
    # การเชื่อมรายการเข้าด้วยกันก็ทำได้ง่าย ใช้ extend
    # การเพิ่มรายการ
    # เพิ่มข้อมูลต่อท้ายรายการ
    # แยกรายการ เมื่อรู้ว่ามีองค์ประกอบจำนวนเท่าใด
    # ใช้ underscore สำหรับค่าที่ไม่สนใจ

--- hello.py
+++ hello.py
@@ -1,31 +1,30 @@
-# List
-integer_list = [1, 2, 3]
-heterogeneous_list = ["string", 0.1, True]
-list_of_list = [integer_list, heterogeneous_list, []]
+# คำสั่ง in เพื่อตรวจสอบการเป็นสมาชิกในรายการ
 
-list_length = len(integer_list)
-list_sum = sum(integer_list)
-# print(list_sum)
+tt = 1 in [1, 2, 3]
+0 in [1, 2, 3]
+# print(tt)
 
-x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-zero = x[0]
-one = x[1]
-nine = x[-1]
-# print(nine)
+# การเชื่อมรายการเข้าด้วยกันก็ทำได้ง่าย ใช้ extend
+x = [1, 2, 3]
+# x.extend([4, 5, 6])
 
-"""
-ใน Python, โค้ด `x[:3]` ใช้เพื่อดึงข้อมูลจากรายการ `x` โดยเริ่มต้นจาก index ที่ `0` และหยุดที่ index ที่ `3` (แต่ไม่รวม index ที่ `3`). ดังนั้น, `first_three` จะได้รับค่า `[0, 1, 2]`.
-"""
-first_three = x[:3]
-three_to_end = x[3:]
-one_to_four = x[1:5]
-last_three = x[-3:]
-without_first_and_last = x[1:-1]
-copy_of_x = x[:]
-print(copy_of_x)
-every_third = x[::3]
-""" 
-โค้ด five_to_three = x[5:2:-1] ใน Python หมายถึงการตัด list x โดยเริ่มจาก index 5 ไปยัง index 2 (แต่ไม่รวม index 2) และด้วย step -1 ซึ่งหมายความว่าการกระโดดย้อนกลับไป 1 index ในแต่ละ step.
-"""
-five_to_three = x[5:2:-1]
-print(every_third)
+# การเพิ่มรายการ
+y = x + [4, 5, 6]
+# print(y)
+
+# เพิ่มข้อมูลต่อท้ายรายการ
+x = [1, 2, 3]
+x.append(0)
+y = x[-1]
+z = len(x)
+# print(x)
+# print(y)
+# print(z)
+
+# แยกรายการ เมื่อรู้ว่ามีองค์ประกอบจำนวนเท่าใด
+x, y = [1, 2]
+# print(x, y)
+
+# ใช้ underscore สำหรับค่าที่ไม่สนใจ
+_, y = [1, 2]
+# print(_)
