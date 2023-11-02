d1fc797b74b859397f6f2fc9b6eb516cb910ff65
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2023-11-02 07:55:30 +0700

    Dictionary 02
    # ใช้ method get ส่งค่าเริ่มต้นกลับมา เมื่อค่าคีย์ไม่อยู่ใน Dictionary
    # กำหนดคีย์ / ค่า ใน []
    # ใช้ Dictionary เก็บข้อมูลที่มีโครงสร้าง
    # ดู key ทั้งหมด

--- hello.py
+++ hello.py
@@ -16,3 +16,31 @@ except KeyError:
 # ตรวจสอบการมีอยู่ของคีย์ได้โดยใช้คำสั่งนี้
 joel_has_grade = "Joel" in grades  # True
 kate_has_grade = "Kate" in grades  # False
+
+
+# ใช้ method get ส่งค่าเริ่มต้นกลับมา เมื่อค่าคีย์ไม่อยู่ใน Dictionary
+joels_grade = grades.get("Joel", 0)  # equals 80
+kates_grade = grades.get("Kate", 0)  # equals 0
+no_ones_grade = grades.get("No One")  # default is None
+
+# กำหนดคีย์ / ค่า ใน []
+grades["Tim"] = 99  # replaces the old value
+grades["Kate"] = 100  # adds a third entry
+num_students = len(grades)  # equals 3
+
+# ใช้ Dictionary เก็บข้อมูลที่มีโครงสร้าง
+tweet = {
+    "user": "joelgrus",
+    "text": "Data Science is Awesome",
+    "retweet_count": 100,
+    "hashtags": ["#data"]
+}
+
+# ดู key ทั้งหมด
+tweet_keys = tweet.keys()  # iterable for the keys
+tweet_values = tweet.values()  # iterable for the values
+tweet_items = tweet.items()  # iterable for the (key, value) tuples
+
+"user" in tweet_keys  # True, but not Pythonic
+"user" in tweet  # Pythonic way of checking for keys
+"joelgrus" in tweet_values  # True (slow but the only way to check)
