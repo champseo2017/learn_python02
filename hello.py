5f6ac5f5abfb9ddc7d86f8c4ac1c9d9595978925
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-01-14 11:00:12 +0700

    for สามารถใช้ผลลัพธ์จาก for ก่อนหน้าได้

--- hello.py
+++ hello.py
@@ -1,23 +1,9 @@
 
-# ค่าความจริง
-# ค่าความจริงใน python ทำงานเหมือนภาษาอื่นๆ ส่วนใหญ่ แต่ต้องใช้ตัวพิมพ์ใหญ่
-one_is_less_than_two = 1 < 2  # equals True
-true_equals_false = True == False  # equals False
-
-# Python ใช้ค่า None เพื่อระบุการไม่มีค่า (คล้ายกับ null ภาษาอื่นเช่นกัน)
-""" 
-ทั้งสองคำสั่ง assert ใช้เพื่อ debugging และการทดสอบเงื่อนไขต่างๆ ในโค้ด เพื่อให้แน่ใจว่าค่าหรือสถานะของตัวแปรนั้นตรงตามที่ต้องการ. แต่คำสั่ง assert x is None เป็นวิธีที่แนะนำเพราะ is ใช้ตรวจสอบ identity ซึ่งเหมาะสมกว่าในการตรวจสอบ None.
-"""
-x = None
-assert x == None
-assert x is None
-
-s = "some_function_that_returns_a_string()"
-if s:
-    first_char = s[0]
-else:
-    first_char = ""
-
-# and จะคืนค่าที่สอง หากสิ่งแรกเป็น จริง และ คืนค่าแรก ถ้าไม่เป็นจริง
-first_char = s and s[0]
-print(first_char)
+# for สามารถใช้ผลลัพธ์จาก for ก่อนหน้าได้
+# สร้างรายการของคู่ตัวเลข (x, y) โดยที่ x และ y เป็นจำนวนเต็มและ y > x
+increasing_pairs = [
+    (x, y)  # สร้างคู่ตัวเลข (x, y)
+    for x in range(10)  # วนลูป x จาก 0 ถึง 9
+    for y in range(x + 1, 10)  # วนลูป y จาก x + 1 ถึง 9 สำหรับแต่ละค่าของ x
+]
+print(increasing_pairs)  # แสดงรายการของคู่ตัวเลขที่มีค่าเพิ่มขึ้น
